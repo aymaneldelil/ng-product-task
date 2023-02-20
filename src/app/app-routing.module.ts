4d2d5ddbea52cd9f0d1516195c406fcd890f0eaf
aymanyori <aymanyori00@gmail.com>
2023-02-20 11:50:16 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//---------------------------------------------------------------------------------------------------------------------------------------------
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./view/product-list/product-list.module').then(
+        (t) => t.ProductListModule
+      ),
+  },
+  {
+    path: 'product/:id',
+    loadChildren: () =>
+      import('./view/product-details/product-details.module').then(
+        (t) => t.ProductDetailsModule
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./view/page404/page404.module').then((t) => t.Page404Module),
+  },
+];
+//---------------------------------------------------------------------------------------------------------------------------------------------
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
