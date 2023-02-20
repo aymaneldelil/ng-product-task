07c94c96ed5350cc50dea0345524c5998370b319
aymanyori <aymanyori00@gmail.com>
2023-02-20 16:18:48 +0200

    Enhance Ui

--- src/app/feature/layout/layout.module.ts
+++ src/app/feature/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HNavbarComponent } from './h-navbar/h-navbar.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -11,7 +11,9 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+RouterModule 
+
   ],
   exports:[
     HNavbarComponent,
