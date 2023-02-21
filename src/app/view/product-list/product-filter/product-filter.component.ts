3e0bbecfa3fce8f604e856e565b87eeb45b78f9a
aymanyori <aymanyori00@gmail.com>
2023-02-21 18:15:31 +0200

    enhance filter

--- src/app/view/product-list/product-filter/product-filter.component.ts
+++ src/app/view/product-list/product-filter/product-filter.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter, map, Observable, startWith, tap, mergeMap } from 'rxjs';
+import { filter, map, Observable, startWith, tap, mergeMap, debounceTime } from 'rxjs';
 import { Iproduct } from 'src/app/core/interfaces/iproduct';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -15,7 +15,7 @@ export interface iProductSearch {
   styleUrls: ['./product-filter.component.scss'],
 })
 export class ProductFilterComponent implements OnInit {
-  
+
   public product_fc = new FormControl('');
   public options: Array<iProductSearch> = [];
   @Output() filteredProduct = new EventEmitter<string>();
@@ -46,7 +46,12 @@ export class ProductFilterComponent implements OnInit {
   }
   //--------------------------------------------------------------------------------------------------------------------------------------------
   private filterProduct(): void {
-    this.product_fc.valueChanges.subscribe({
+    this.product_fc.valueChanges.pipe(
+      debounceTime(1000),
+      map((m)=>{
+        return m?.toLocaleLowerCase()
+      })
+    ).subscribe({
       next: (n) => {
         this.filteredProduct.emit(n!)
       },
