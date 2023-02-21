89185bb72ad4812e88ae363370edfd1c6b826f96
aymanyori <aymanyori00@gmail.com>
2023-02-21 15:03:08 +0200

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './feature/layout/layout.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
 
-imports: [BrowserModule, AppRoutingModule, HttpClientModule ,LayoutModule ],
+imports: [BrowserModule, AppRoutingModule, HttpClientModule ,LayoutModule, BrowserAnimationsModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
