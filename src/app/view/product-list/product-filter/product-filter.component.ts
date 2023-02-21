3ff0b786cb809a62c90e70359d876ba6c2216182
aymanyori <aymanyori00@gmail.com>
2023-02-21 21:54:50 +0200

    Product Filter

--- src/app/view/product-list/product-filter/product-filter.component.ts
+++ src/app/view/product-list/product-filter/product-filter.component.ts
@@ -1,13 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter, map, Observable, startWith, tap, mergeMap, debounceTime } from 'rxjs';
-import { Iproduct } from 'src/app/core/interfaces/iproduct';
+import { map, mergeMap, debounceTime } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
 
-export interface iProductSearch {
-  id: number;
-  title: string;
-}
 
 @Component({
   selector: 'app-product-filter',
@@ -17,32 +12,12 @@ export interface iProductSearch {
 export class ProductFilterComponent implements OnInit {
 
   public product_fc = new FormControl('');
-  public options: Array<iProductSearch> = [];
   @Output() filteredProduct = new EventEmitter<string>();
   //---------------------------------------------------------------------------------------------------------------------------------------------
   constructor(private _productSVC: ProductService) {}
   //---------------------------------------------------------------------------------------------------------------------------------------------
   ngOnInit() {
-    this.initialValues();
     this.filterProduct();
-  }
-  //---------------------------------------------------------------------------------------------------------------------------------------------
-  private initialValues(): void {
-    this._productSVC
-      .getProducts()
-      .pipe(
-        mergeMap((m) => {
-          return m.products;
-        }),
-        map((f) => {
-          return { id: f.id, title: f.title };
-        })
-      )
-      .subscribe({
-        next: (res) => {
-          this.options.push(res);
-        },
-      });
   }
   //--------------------------------------------------------------------------------------------------------------------------------------------
   private filterProduct(): void {
